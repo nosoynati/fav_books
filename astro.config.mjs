3072fc50310334de9a73cad633fa9a506e92f67e
Natasha <shotaoouji@gmail.com>
2026-01-08 13:01:51 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   // ...
   integrations: [tailwind()],
   // ...
+  env: {
+    schema: {
+      SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public'}),
+      SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public'})
+    }
+  }
 });
