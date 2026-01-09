d075b9d75d1f3dea128039aa3d58ee618ea43bb5
Natasha <shotaoouji@gmail.com>
2026-01-09 10:34:37 -0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   // ...
   env: {
     schema: {
-      SHOW_BUY_BUTTON: envField.boolean({ context: 'server', access: 'public'}),
+      SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public'}),
       SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public'})
     }
   }
