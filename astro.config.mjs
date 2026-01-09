19755a268d68858c915de9dd48e07b22fa3d7dd7
Natasha <shotaoouji@gmail.com>
2026-01-09 18:01:18 -0300

    add: server islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   // ...
+  output: 'server',
+
   integrations: [tailwind()],
+
   // ...
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({ default: true, context: 'server', access: 'public'}),
       SCORE_API_ENDPOINT: envField.string({ context: 'server', access: 'public'})
     }
-  }
+  },
+
+  adapter: vercel()
 });
